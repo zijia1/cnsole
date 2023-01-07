b4db4f22f9470147adaefaea4bc27ddff36e5398
zijia1 <107959442+zijia1@users.noreply.github.com>
2023-01-07 10:29:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name = "Zac";
+var last_name = "yang";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 200;
+var b = 300;
+var subtract = math.subtract(a, b);
+console.log(subtract);
